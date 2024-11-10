56f2bbde5635a1e53282a6c3669810734ca35da0
AnaZaninovic <anazaninovic37@gmail.com>
2024-11-10 20:30:01 +0100

    cors

--- src/pages/api/getToken.ts
+++ src/pages/api/getToken.ts
@@ -1,11 +1,23 @@
 import type {NextApiRequest, NextApiResponse} from 'next'
 
 import { randomUUID, UUID } from 'crypto';
+import { PAGE_1, PAGE_2 } from '@/config';
 
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if(req.method == "OPTIONS"){
+        res.setHeader('Access-Control-Allow-Origin', PAGE_1);
+        res.setHeader('Access-Control-Allow-Origin', PAGE_2);
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+        res.setHeader('Access-Control-Allow-Headers', 'Content-Type, CSRF-Token');
+        res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+        res.status(200).end();
+        return 
+    }
+
     console.table(req.cookies);
 
     const myCookie = req.cookies.myCookie;

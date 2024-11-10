924d402761429552383e23e1a14de6dcef0e8554
AnaZaninovic <anazaninovic37@gmail.com>
2024-11-10 20:58:40 +0100

    cors

--- src/pages/api/getToken.ts
+++ src/pages/api/getToken.ts
@@ -9,7 +9,7 @@ export default async function handler(
 ) {
     res.setHeader('Access-Control-Allow-Origin', req.headers.origin || PAGE_1);
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, CSRF-Token');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, CSRF-Token, Cookie');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
 
 
